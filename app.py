59b6c45e83bfd79bc42f0fd510df016801958dd8
Alfred Kinyua <alfiemarquis@gmail.com>
2017-05-26 00:11:34 +0300

    Added a limit and a skip to mongo queries Just before the old articles are deleted, 9pm UTC, there are over 600 articles and we needed a way to improve the results by a factor of around 20

--- app.py
+++ app.py
@@ -10,45 +10,63 @@ client = MongoClient(os.environ['MongoDB_URI'])
 db = client['kenya-news']  # Select the database
 collection = db.news
 
+"""
+Api end points you can use with your server
+
+Added skip in url so it can be passed to mongo query
+So the request can have a skip parameter if it's requesting more than the 30 limited in 1st response
+"""
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
-@app.route('/tuko')
-def tuko():
-    tuko_docs = [doc for doc in collection.find({'source': 'tuko'}, {'_id': 0}).sort('date', -1)]
+@app.route('/tuko', defaults={'skip': 0})
+@app.route('/tuko/<int:skip>')
+def tuko(skip):
+    tuko_docs = [doc for doc in collection.find({
+        'source': 'tuko'}, {'_id': 0}).skip(skip).limit(30).sort('date', -1)]
     return jsonify(tuko_docs)
 
 
-@app.route('/capital')
-def capital():
-    capital_docs = [doc for doc in collection.find({'source': 'capital'}, {'_id': 0}).sort('date', -1)]
+@app.route('/capital', defaults={'skip': 0})
+@app.route('/capital/<int:skip>')
+def capital(skip):
+    capital_docs = [doc for doc in collection.find({
+        'source': 'capital'}, {'_id': 0}).skip(skip).limit(30).sort('date', -1)]
     return jsonify(capital_docs)
 
 
-@app.route('/nation')
-def nation():
-    nation_docs = [doc for doc in collection.find({'source': 'nation'}, {'_id': 0}).sort('date', -1)]
+@app.route('/nation', defaults={'skip': 0})
+@app.route('/nation/<int:skip>')
+def nation(skip):
+    nation_docs = [doc for doc in collection.find({
+        'source': 'nation'}, {'_id': 0}).skip(skip).limit(30).sort('date', -1)]
     return jsonify(nation_docs)
 
 
-# @app.route('/the-star')
-# def the_star():
-#     the_star_docs = [doc for doc in collection.find({'source': 'the_star'}, {'_id': 0}).sort('date', -1)]
-#     return jsonify(the_star_docs)
+@app.route('/the-star', defaults={'skip': 0})
+@app.route('/the-star/<int:skip>')
+def the_star(skip):
+    the_star_docs = [doc for doc in collection.find({
+        'source': 'the_star'}, {'_id': 0}).skip(skip).limit(30).sort('date', -1)]
+    return jsonify(the_star_docs)
 
 
-@app.route('/standard')
-def standard():
-    standard_docs = [doc for doc in collection.find({'source': 'standard'}, {'_id': 0}).sort('date', -1)]
+@app.route('/standard', defaults={'skip': 0})
+@app.route('/standard/<int:skip>')
+def standard(skip):
+    standard_docs = [doc for doc in collection.find({
+        'source': 'standard'}, {'_id': 0}).skip(skip).limit(30).sort('date', -1)]
     return jsonify(standard_docs)
 
 
-@app.route('/latest-news')
-def latest_news():
-    news = [doc for doc in collection.find({}, {'_id': 0}).sort('date', -1)]
+@app.route('/latest-news/', defaults={'skip': 0})
+@app.route('/latest-news/<int:skip>')
+def latest_news(skip):
+    news = [doc for doc in collection.find({}, {'_id': 0}).skip(skip).limit(30).sort('date', -1)]
     return jsonify(news)
 
 
